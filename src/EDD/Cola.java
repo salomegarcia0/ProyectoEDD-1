add096a5d39b21f41a431714cacbd3ecd1fb00f0
salomegarcia0 <salome.garcia@correo.unimet.edu.ve>
2024-10-16 15:24:46 -0400

    ale

--- src/EDD/Cola.java
+++ src/EDD/Cola.java
@@ -5,9 +5,91 @@
 package EDD;
 
 /**
- *
- * @author Aleida Vaamonde
- */
+*
+* @author Alejandra Oporto
+*/
 public class Cola {
     
+    private Nodo inicio;
+    private Nodo cola;
+    private int medida;
+
+    public Cola() {
+        this.inicio = null;
+        this.cola = null;
+        this.medida = 0;
+    }
+
+    public Nodo getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Nodo inicio) {
+        this.inicio = inicio;
+    }
+
+    public Nodo getCola() {
+        return cola;
+    }
+
+    public void setCola(Nodo cola) {
+        this.cola = cola;
+    }
+
+    public int getMedida() {
+        return medida;
+    }
+
+    public void setMedida(int medida) {
+        this.medida = medida;
+    }
+
+    public boolean colaVacia() {
+        return this.inicio == null;
+    }
+
+    public void enColar(Object dato) {
+        Nodo pNew = new Nodo(dato);
+        if (this.colaVacia()) {
+            this.setInicio(pNew);
+            this.setCola(pNew);
+        } else {
+            this.cola.setnext(pNew);
+            this.setCola(pNew);
+        }
+        medida++;
+    }
+
+    public Object desEnColar() {
+        if (this.colaVacia()) {
+            Object quitar = this.inicio.getData();
+            this.setInicio(null);
+            this.setCola(null);
+            medida--;
+            return quitar;
+
+        } else {
+            Object quitar = this.inicio.getData();
+            this.setInicio(this.inicio.getnext());
+            medida--;
+            return quitar;
+        }
+    }
+
+    public void destruir() {
+        inicio = null;
+        cola = null;
+        medida = 0;
+    }
+
+    //Mostrar los elementos de una Pila}
+    public void listar() {
+        Nodo aux = inicio;
+        String pila = "COLA:\n";
+        while (aux != null) {
+            pila = pila + aux.getData() + "\n";
+            aux = aux.getnext();
+        }
+        System.out.println(pila);
+    }
 }
--- src/EDD/Grafo.java
+++ src/EDD/Grafo.java
@@ -4,10 +4,160 @@
  */
 package EDD;
 
+import Clases.Estacion;
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author Aleida Vaamonde
+ * @author Alejandra Oporto
+ */
+
+/**
+ * Clase Grafo que representa un grafo no dirigido, donde los nodos son estaciones de un sistema de transporte.
+ * Utiliza una lista enlazada simple para almacenar las estaciones y sus conexiones.
  */
 public class Grafo {
+
+    private ListaSimple estaciones;
     
+    public Grafo(){
+        this.estaciones = new ListaSimple();
+    }
+    
+    public Grafo(ListaSimple estaciones) {
+        this.estaciones = estaciones;
+    }
+    
+       public boolean grafoVacio(){
+        return this.estaciones.isEmpty();
+    }
+
+    public ListaSimple getEstaciones() {
+        return estaciones;
+    }
+
+    public void setEstaciones(ListaSimple estaciones) {
+        this.estaciones = estaciones;
+    }
+    
+    
+    /**
+     * Busca una estación en el grafo por su nombre.
+     *
+     * @param nombre El nombre de la estación a buscar.
+     * @return La estación encontrada, o null si no existe.
+     */
+    public Estacion encontrarEstacion(String nombre) {
+        if (this.estaciones.isEmpty()) {
+            return null;
+        }
+        String nombreLower = nombre.toLowerCase();
+
+        for (int i = 0; i < this.estaciones.getSize(); i++) {
+            Estacion estacion = (Estacion) this.estaciones.getValor(i);
+            String nombreEstacionLower = estacion.getNombreEstacion().toLowerCase();
+
+            if (nombreEstacionLower.equals(nombreLower)) {
+                return estacion;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Agrega una nueva estación al grafo, verificando que no exista previamente.
+     * Asigna un número de estación único a la nueva estación.
+     *
+     * @param estacion La estación a agregar.
+     */
+    
+    public void agregarEstacion(Estacion estacion){
+        if(encontrarEstacion(estacion.getNombreEstacion())!= null){
+            int idEstacion = this.estaciones.getSize();
+            estacion.setIdEstacion(idEstacion);
+            this.estaciones.aggFinal(estacion);
+            
+            JOptionPane.showMessageDialog(null, "Estacion creada correctamente.");
+        }else{
+            JOptionPane.showMessageDialog(null, "Estacion existente");
+        }
+    }
+    
+    /**
+     * Funcion para verificar si las estaciones son validas o no <code>null</code>
+     */
+    private boolean sonEstacionesValidas(Estacion estacion1, Estacion estacion2){
+        return estacion1 != null && estacion2 != null;
+    }
+    
+    public void agregarConexion(String nombreE1, String nombreE2){
+        Estacion estacion1 = this.encontrarEstacion(nombreE1);
+        Estacion estacion2 = this.encontrarEstacion(nombreE2);
+        
+        if(sonEstacionesValidas(estacion1, estacion2)){
+            estacion1.getListaAdyacencia().aggFinal(estacion1);
+            estacion2.getListaAdyacencia().aggFinal(estacion2);
+            
+            JOptionPane.showMessageDialog(null, "Conexion establecida exitosamente");
+        }else{
+            JOptionPane.showMessageDialog(null, "Error en agregar la conexion,"
+                    + "asegurese de escribir ambas estaciones correctamente");
+        }
+    }
+    
+    public ListaSimple getAdyacentes(String nombreEstacion){
+        Estacion estacion = this.encontrarEstacion(nombreEstacion);
+        if(estacion != null){
+            return estacion.getListaAdyacencia();
+        }else{
+            return null;
+        }
+    }
+    
+     private boolean verificarConexiones(ListaSimple adyacentes, Estacion estacion2) {
+        for (int i = 0; i < adyacentes.getSize(); i++) {
+            if (adyacentes.getValor(i) == estacion2) {
+               return true;
+            }
+        }
+        return false;
+    }
+     
+      public boolean estanConectadas(String nombreE1, String nombreE2){
+        Estacion estacion1 = this.encontrarEstacion(nombreE1);
+        Estacion estacion2 = this.encontrarEstacion(nombreE2);
+        
+        if (sonEstacionesValidas(estacion1, estacion2)){
+            ListaSimple adyacentes = estacion1.getListaAdyacencia();
+            return verificarConexiones(adyacentes, estacion2);    
+            }
+            return false;
+    }
+    
+     private String construirGrafoString() {
+        StringBuilder strGrafo = new StringBuilder();
+        for (int i = 0; i < this.estaciones.getSize(); i++) {
+            Estacion estacion = (Estacion) estaciones.getValor(i);
+            strGrafo.append(estacion.getNombreEstacion())
+                    .append(" -> ")
+                    .append(estacion.ConvertirListaAdyacencia())
+                    .append("\n");
+        }
+        return strGrafo.toString();
+    }
+
+    public void mostrarGrafo() {
+        if (!estaciones.isEmpty()) {
+            String strGrafo = construirGrafoString();
+            JOptionPane.showMessageDialog(null, strGrafo);
+        } else {
+            JOptionPane.showMessageDialog(null, "El grafo no presenta estaciones.");
+        }
+    }
+
+    
+    
+
+
 }
